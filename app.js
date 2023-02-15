8b0732e685a03e66ecc720a74b8b473ba1e98116
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-02-15 22:55:22 +0500

    add express.js and create testing API

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World.');
+import express from 'express'
+
+const app = express()
+
+app.use('/', (req, res, next) => {
+    res.send('Hello world')
+})
+
+app.listen(5000, () => {
+    console.log('Server start on PORT 5000')
+})
