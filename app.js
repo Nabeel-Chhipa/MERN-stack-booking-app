aefcf48ac361f1bdbe5caba1911ce294c4a57343
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-02-15 23:34:59 +0500

    create mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-    res.send('Hello world')
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.hnj2ho3.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(5000, () => {
+        console.log('Server start on PORT 5000 and connected to MongoDB.')
+    })
 })
-
-app.listen(5000, () => {
-    console.log('Server start on PORT 5000')
+.catch((e) => {
+    console.log(e);
 })
