851abfc152981d5897a94072a194196f007f748e
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-02-20 22:14:33 +0500

    create admin login API

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,6 +3,9 @@ import bcrypt from 'bcryptjs';
 
 export const addAdmin = async (req, res, next) => {
     const { email, password } = req.body;
+    if(!email && email.trim()==='' && !password && password.trim()==='') {
+        return res.status(422).json({message: 'invalid inputs'})
+    }
     let existingUser;
     try {
         existingUser = await Admin.findOne({ email })
@@ -27,4 +30,25 @@ export const addAdmin = async (req, res, next) => {
         return res.status(500).json({ message: 'Unable to find admin' })
     }
     return res.status(201).json({ admin })
+}
+
+export const adminLogin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if(!email && email.trim()==='' && !password && password.trim()==='') {
+        return res.status(422).json({message: 'invalid inputs'})
+    }
+    let existingUser;
+    try {
+        existingUser = await Admin.findOne({ email })
+    } catch (error) {
+        return console.log(error)
+    }
+    if(!existingUser) {
+        return res.status(400).json({message: 'Admin not found'})
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password)
+    if(!isPasswordCorrect) {
+        return res.status(400).json({message:'Incorrect password'})
+    }
+    return res.status(200).json({message:'Authentication completed'})
 }
--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { addAdmin } from '../controllers/admin-controller'
+import { addAdmin, adminLogin } from '../controllers/admin-controller'
 
 const adminRouter = express.Router()
 
 adminRouter.post('/signup', addAdmin)
+adminRouter.post('/login', adminLogin)
 
 export default adminRouter
