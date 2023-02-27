c8ea82571455334830471e7551422a611e8f9d80
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-02-28 00:18:30 +0500

    create header and get all movies from db

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { AppBar, Tab, Tabs, TextField, Toolbar } from "@mui/material";
+import MovieIcon from "@mui/icons-material/Movie";
+import { Box } from "@mui/system";
+import Autocomplete from "@mui/material/Autocomplete";
+import { getAllMovies } from "../api-helpers/api-helpers";
+
+const Header = () => {
+  const [value, setValue] = useState(0);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getAllMovies()
+      .then((data) => setMovies(data.movies))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <AppBar sx={{ bgcolor: "#999" }}>
+      <Toolbar>
+        <Box width={"20%"}>
+          <MovieIcon />
+        </Box>
+        <Box width={"30%"} margin={"auto"}>
+          <Autocomplete
+            freeSolo
+            options={movies && movies?.map((movie) => movie.title)}
+            renderInput={(params) => (
+              <TextField
+                sx={{ input: { color: "white" } }}
+                variant="standard"
+                {...params}
+                placeholder="search movie"
+              />
+            )}
+          />
+        </Box>
+        <Box display={"flex"}>
+          <Tabs
+            textColor="white"
+            value={value}
+            indicatorColor={"secondary"}
+            onChange={(e, eValue) => setValue(eValue)}
+          >
+            <Tab label="Movies" />
+            <Tab label="Admin" />
+            <Tab label="Auth" />
+          </Tabs>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
