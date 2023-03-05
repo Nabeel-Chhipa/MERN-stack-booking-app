fc4aa88596d67732f55f776a581705f96886be22
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-03-06 00:07:16 +0500

    implement authentication with redux toolkil

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Admin from "./components/Admin/Admin";
 import Auth from "./components/Auth/Auth";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import Movies from "./components/Movies/Movies";
+import { adminAction, userAction } from "./store";
 
 function App() {
 
+  const dispatch = useDispatch()
   const isAdminLoggedIn = useSelector(state => state.admin.isLoggedIn)
   const isUserLoggedIn = useSelector(state => state.user.isLoggedIn)
   console.log('isAdminLoggedIn', isAdminLoggedIn)
   console.log('isUserLoggedIn', isUserLoggedIn)
+
+  useEffect(() => {
+    if (localStorage.getItem('userId')) {
+      dispatch(userAction.login())
+    } else if(localStorage.getItem('adminId')) {
+      dispatch(adminAction.login())
+    }
+  }, [])
 
   return (
     <div>
